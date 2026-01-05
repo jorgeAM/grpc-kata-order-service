309e90f4fe745d75ae6896b9b85b1514aeb6ec16
Jorge L. <jorge.alfmur@gmail.com>
2026-01-05 17:24:57 -0500

    test: integration tests

--- database/migration/000002_create_order_items.up.sql
+++ database/migration/000002_create_order_items.up.sql
@@ -3,7 +3,7 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS order_schema.order_items
 (
     id uuid PRIMARY KEY,
-    order_id VARCHAR(50) NOT NULL,
+    order_id uuid NOT NULL,
     product_code VARCHAR(50) NOT NULL,
     quantity INT NOT NULL,
     unit_price DECIMAL(10,2) NOT NULL,
